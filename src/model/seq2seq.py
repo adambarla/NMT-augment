0b913b7ef727349acb03520b3c6c4934f3df2f94
Adam Barla <barlaada@fit.cvut.cz>
2024-04-04 06:10:49 +0800

    feat(seq2seq): added translation capability to the model and some prints of tranlations during evaluation, closes #28

--- src/model/seq2seq.py
+++ src/model/seq2seq.py
@@ -1,18 +1,21 @@
 import torch
+import torch.nn.functional as F
 from torch import nn, Tensor
 
 
 class Seq2Seq(nn.Module):
     def __init__(
-        self,
-        src_tok_emb,
-        tgt_tok_emb,
-        positional_encoding,
-        transformer,
-        generator,
-        device,
-        pad_token,
-        **kwargs
+            self,
+            src_tok_emb,
+            tgt_tok_emb,
+            positional_encoding,
+            transformer,
+            generator,
+            device,
+            pad_token_id,
+            bos_token_id,
+            eos_token_id,
+            **kwargs
     ):
         super(Seq2Seq, self).__init__()
         self.transformer = transformer
@@ -21,14 +24,16 @@ class Seq2Seq(nn.Module):
         self.positional_encoding = positional_encoding
         self.generator = generator
         self.device = device
-        self.pad_token = pad_token
+        self.pad_token = pad_token_id
+        self.bos_token = bos_token_id
+        self.eos_token = eos_token_id
 
     def forward(self, src: Tensor, tgt: Tensor):
         # src: Length x Batch
         # tgt: (L-1) x B
         src_emb = self.positional_encoding(self.src_tok_emb(src))
         tgt_emb = self.positional_encoding(self.tgt_tok_emb(tgt))
-        src_mask, tgt_mask, src_padding_mask, tgt_padding_mask = self.create_mask(
+        src_mask, tgt_mask, src_padding_mask, tgt_padding_mask = self._create_mask(
             src, tgt
         )
         outs = self.transformer(
@@ -41,6 +46,7 @@ class Seq2Seq(nn.Module):
             tgt_padding_mask,
             src_padding_mask,
         )
+        # return = L x B x Vocab
         return self.generator(outs)
 
     def encode(self, src: Tensor, src_mask: Tensor):
@@ -53,7 +59,25 @@ class Seq2Seq(nn.Module):
             self.positional_encoding(self.tgt_tok_emb(tgt)), memory, tgt_mask
         )
 
-    def generate_square_subsequent_mask(self, sz):
+    def translate(self, input, max_length=torch.inf, context_size=None):
+        i = 0
+        output = torch.ones(1, input.shape[1]).fill_(self.bos_token).type(torch.long).to(self.device)
+        finished = torch.zeros(input.shape[1]).bool().to(self.device)
+        while i < max_length:
+            i += 1
+            context = output[:, -context_size:] if context_size is not None else output
+            logits = self.forward(input, context)
+            logits = logits[-1, :, :]
+            probs = F.softmax(logits, dim=-1)
+            output_next = torch.multinomial(probs, num_samples=1).transpose(0,1)
+            output = torch.cat([output, output_next], dim=0)
+            # Update finished mask where output_next is eos_token
+            finished |= (output_next.squeeze() == self.eos_token)
+            if finished.all():
+                break # Break if all sequences have received an EOS token
+        return output
+
+    def _generate_square_subsequent_mask(self, sz):
         mask = (torch.triu(torch.ones((sz, sz), device=self.device)) == 1).transpose(
             0, 1
         )
@@ -64,15 +88,13 @@ class Seq2Seq(nn.Module):
         )
         return mask
 
-    def create_mask(self, src, tgt):
+    def _create_mask(self, src: Tensor, tgt: Tensor):
         src_seq_len = src.shape[0]
         tgt_seq_len = tgt.shape[0]
-
-        tgt_mask = self.generate_square_subsequent_mask(tgt_seq_len)
+        tgt_mask = self._generate_square_subsequent_mask(tgt_seq_len)
         src_mask = torch.zeros((src_seq_len, src_seq_len), device=self.device).type(
             torch.bool
         )
-
         src_padding_mask = (src == self.pad_token).transpose(0, 1)
         tgt_padding_mask = (tgt == self.pad_token).transpose(0, 1)
         return src_mask, tgt_mask, src_padding_mask, tgt_padding_mask
--- src/training.py
+++ src/training.py
@@ -19,7 +19,7 @@ from accelerate import Accelerator
 
 
 def epoch_train(
-    model, loader, optimizer, criterion, device, accelerator, scheduler=None
+        model, loader, optimizer, criterion, device, accelerator
 ):
     epoch_loss = 0.0
 
@@ -30,24 +30,22 @@ def epoch_train(
             inputs, targets = batch
             inputs = inputs.transpose(0, 1).to(device)
             targets = targets.transpose(0, 1).to(device)
-
             outputs = model(inputs, targets[:-1, :])
             loss = criterion(
                 outputs.reshape(-1, outputs.shape[-1]), targets[1:, :].reshape(-1)
             )
             accelerator.backward(loss)
             optimizer.step()
-            # todo: add scheduler
-            # scheduler.step()
             epoch_loss += accelerator.gather(loss.item())
-            pbar.set_description(f"Train Loss: {epoch_loss/(i+1.0):.3f}")
+            pbar.set_description(f"Train Loss: {epoch_loss / (i + 1.0):.3f}")
             pbar.update(1)
     return epoch_loss / len(loader)
 
 
-def epoch_evaluate(model, loader, criterion, device, accelerator):
+def epoch_evaluate(model, loader, criterion, device, accelerator, tokenizer):
     epoch_loss = 0.0
     model.eval()
+    inputs = targets = None
     with torch.no_grad():
         with tqdm(total=len(loader), desc="Training Progress") as pbar:
             for i, batch in enumerate(loader):
@@ -59,24 +57,31 @@ def epoch_evaluate(model, loader, criterion, device, accelerator):
                     outputs.reshape(-1, outputs.shape[-1]), targets[1:, :].reshape(-1)
                 )
                 epoch_loss += accelerator.gather(loss.item())
-                pbar.set_description(f"Valid Loss: {epoch_loss/(i+1.0):.3f}")
+                pbar.set_description(f"Valid Loss: {epoch_loss / (i + 1.0):.3f}")
                 pbar.update(1)
+        translations = model.translate(inputs, max_length=inputs.shape[0]*1.05, context_size=inputs.shape[0])
+        for i in range(3):
+            print(  f"\n\t input: {tokenizer.decode(inputs[:,i])[0]}\n"
+                    f"\ttarget: {tokenizer.decode(targets[:,i])[0]}\n"
+                    f"\toutput: {tokenizer.decode(translations[:,i])[0]}"
+                    )
     return epoch_loss / len(loader)
 
 
 def train(
-    device,
-    model,
-    optimizer,
-    criterion,
-    accelerator,
-    n_epochs,
-    train_loader,
-    val_loader,
-    test_loader,
+        device,
+        model,
+        optimizer,
+        criterion,
+        accelerator,
+        n_epochs,
+        train_loader,
+        val_loader,
+        test_loader,
+        tokenizer
 ):
     for epoch in range(n_epochs):
-        # todo: add scheduler
+        print(f"Epoch: {epoch + 1:>{len(str(n_epochs))}d}/{n_epochs}")
         train_loss = epoch_train(
             model,
             train_loader,
@@ -84,12 +89,11 @@ def train(
             criterion,
             device,
             accelerator,
-            scheduler=None,
         )
-        valid_loss = epoch_evaluate(model, val_loader, criterion, device, accelerator)
-
+        valid_loss = epoch_evaluate(model, val_loader, criterion, device, accelerator, tokenizer)
         wandb.log({"train_loss": train_loss, "valid_loss": valid_loss})
-    test_loss = epoch_evaluate(model, test_loader, criterion, device, accelerator)
+        print('\n'+'-' * (len(str(n_epochs))*2+8))
+    test_loss = epoch_evaluate(model, test_loader, criterion, device, accelerator, tokenizer)
     print(f" Test Loss: {test_loss:.3f} ")
     wandb.log({"test_loss": test_loss})
 
@@ -117,7 +121,11 @@ def main(cfg):
         train_loader, val_loader, test_loader
     )
     model = hydra.utils.instantiate(
-        cfg.model, device=device, pad_token=tokenizer.pad_token_id
+        cfg.model,
+        device=device,
+        pad_token_id=tokenizer.pad_token_id,
+        bos_token_id=tokenizer.bos_token_id,
+        eos_token_id=tokenizer.eos_token_id
     )
     model.to(device)
     print(f"Model:\n{model}")
@@ -136,6 +144,7 @@ def main(cfg):
         train_loader,
         val_loader,
         test_loader,
+        tokenizer
     )
 
 
