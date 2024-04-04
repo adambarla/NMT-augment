e8c6242eaa516b4f5f8933add22dc6b7f24c6d5f
jin010 <jin010@e.ntu.edu.sg>
2024-04-04 21:15:47 +0800

    tested wordtokenizer, can run properly, reduced vocab size with unk

--- src/tokenizer/word_old.py
+++ src/tokenizer/word_old.py
@@ -1,18 +1,12 @@
-from collections import Counter
-
 class WordTokenizer:
-    def __init__(self, dataset, max_vocab_size=10000, min_freq=1, **kwargs):
-        self.max_vocab_size = max_vocab_size
-        self.min_freq = min_freq
+    def __init__(self, dataset, **kwargs):
         self.vocab = self._create_vocab(dataset)
         self._stoi = {w: i for i, w in enumerate(self.vocab)}
         self._itos = {i: w for i, w in enumerate(self.vocab)}
         self.vocab_size = len(self.vocab)
         self.pad_token_id = self._stoi["<pad>"]
-        self.unk_token_id = self._stoi["<unk>"]
-        print(f"Vocabulary size: {self.vocab_size}")
-        print(f"Pad token ID: {self.pad_token_id}")
-        print(f"Unknown token ID: {self.unk_token_id}")
+        print(f"my_Vocabulary size: {self.vocab_size}")
+        print(f"my_Pad token ID: {self.pad_token_id}")
 
     def encode(
         self,
@@ -22,28 +16,35 @@ class WordTokenizer:
         padding="max_length",
         max_length=None,
     ):
+
+        #print(f"Input text: {x}")
+
         words = x.split()
-        encoded = [self._stoi.get(w, self.unk_token_id) for w in words]
+        #print(f"Words: {words}")
+        encoded = [self._stoi.get(w, self._stoi["<unk>"]) for w in words]
+        #print(f"Encoded: {encoded}")
         if truncation and max_length is not None:
             encoded = encoded[: max_length - (2 if add_special_tokens else 0)]
         if add_special_tokens:
             encoded = [self._stoi["<s>"]] + encoded + [self._stoi["</s>"]]
         if padding == "max_length" and max_length is not None:
-            encoded += [self.pad_token_id] * (max_length - len(encoded))
+            encoded += [self._stoi["<pad>"]] * (max_length - len(encoded))
         return encoded
 
     def decode(self, x):
-        decoded_text = " ".join([self._itos.get(w, "<unk>") for w in x])
-        print(f"Decoded text: {decoded_text}")
+        #print(f"Word indices: {x}")
+        decoded_text = " ".join([self._itos[w] for w in x])
+        print(f"my_Decoded text: {decoded_text}")
         return decoded_text
 
     def _create_vocab(self, dataset):
-        word_counts = Counter()
+        words = set()
         for i in dataset:
             for r in dataset[i]:
                 for t in r["translation"]:
-                    word_counts.update(r["translation"][t].split())
-
+                    words.update(r["translation"][t].split())
+        #print(f"Unique words: {words}")
         vocab = ["<s>", "<pad>", "</s>", "<unk>"]
-        vocab.extend([word for word, count in word_counts.most_common(self.max_vocab_size - 4) if count >= self.min_freq])
+        vocab.extend(words)
+        #print(f"Vocabulary: {vocab}")
         return vocab
--- src/training.py
+++ src/training.py
@@ -105,12 +105,17 @@ def main(cfg):
         log_with="wandb",
         # logging_dir="logs" # unexpected argument?
     )
-    device = get_device(cfg)
+    
+    device = get_device(cfg) #torch.device("cuda:1")
     dataset = get_dataset(cfg)
     tokenizer = hydra.utils.instantiate(cfg.tokenizer, dataset=dataset)
     cfg.src_vocab_size = tokenizer.vocab_size
     cfg.tgt_vocab_size = tokenizer.vocab_size
     print(f"Tokenizer:\n{tokenizer}")
+
+    #added by me
+    print(f"my_Vocabulary size: {tokenizer.vocab_size}")
+    print(f"my_Pad token ID: {tokenizer.pad_token_id}")
 
     train_loader, val_loader, test_loader = get_dataloaders(cfg, tokenizer, dataset)
     train_loader, val_loader, test_loader = accelerator.prepare(
