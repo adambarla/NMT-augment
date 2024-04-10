6dc45d81ed48c701db2079ed0546f610bbabb09c
jin010 <jin010@e.ntu.edu.sg>
2024-04-10 17:28:35 +0800

    removed .to(device)

--- src/utils/epoch.py
+++ src/utils/epoch.py
@@ -11,8 +11,8 @@ def epoch_train(model, loader, optimizer, criterion, device, accelerator):
         for i, batch in enumerate(loader):
             optimizer.zero_grad()
             inputs, targets = batch
-            inputs = inputs.transpose(0, 1).to(device)
-            targets = targets.transpose(0, 1).to(device)
+            inputs = inputs.transpose(0, 1)#.to(device)
+            targets = targets.transpose(0, 1)#.to(device)
             outputs = model(inputs, targets[:-1, :])
             loss = criterion(
                 outputs.reshape(-1, outputs.shape[-1]), targets[1:, :].reshape(-1)
@@ -21,8 +21,9 @@ def epoch_train(model, loader, optimizer, criterion, device, accelerator):
             accelerator.backward(loss)
             optimizer.step()
 
-            epoch_loss += torch.mean(accelerator.gather(loss.detach()))
-            pbar.set_description(f"Train Loss: {(epoch_loss / (i + 1.0)).item():.3f}")
+            epoch_loss += torch.mean(accelerator.gather(loss))
+            #epoch_loss += accelerator.reduce(loss, reduction='sum').item()
+            pbar.set_description(f"Train Loss: {(epoch_loss / (i + 1.0)):.3f}")
             pbar.update(1)
     return epoch_loss / len(loader)
 
@@ -46,8 +47,8 @@ def epoch_evaluate(
         with tqdm(total=len(loader), desc="Valid") as pbar:
             for i, batch in enumerate(loader):
                 inputs, targets = batch
-                inputs = inputs.transpose(0, 1).to(device)  # L x B
-                targets = targets.transpose(0, 1).to(device)
+                inputs = inputs.transpose(0, 1)#.to(device)  # L x B
+                targets = targets.transpose(0, 1)#.to(device)
                 outputs = model(inputs, targets[:-1])  # L x B x V
                 loss = criterion(
                     outputs.reshape(-1, outputs.shape[-1]), targets[1:].reshape(-1)
