777304b3eb93833baef98950ccb6f22ffc589fb9
Adam Barla <barlaada@fit.cvut.cz>
2024-04-16 23:41:02 +0800

    fix: bug in patience

--- src/utils/patience.py
+++ src/utils/patience.py
@@ -6,12 +6,18 @@ class Patience:
         self.metric = metric
         self.best = None
         self.counter = 0
+        if mode not in ["min", "max"]:
+            raise ValueError("Mode must be 'min' or 'max'.")
+        if patience < 0:
+            raise ValueError("Patience must be at least 0.")
 
     def should_stop(self, results):
+        if self.metric not in results:
+            raise ValueError(f"Metric {self.metric} not in results.")
         if (
             self.best is None
-            or (self.mode == "min" and self.best is results[self.metric] < self.best)
-            or (self.mode == "max" and self.best is results[self.metric] > self.best)
+            or (self.mode == "min" and results[self.metric] < self.best)
+            or (self.mode == "max" and results[self.metric] > self.best)
         ):
             self.best = results[self.metric]
             self.counter = 0
