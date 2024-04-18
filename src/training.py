fix: broadcast accepts tensors

src/training.py
@@ -1,4 +1,5 @@
 import hydra
+import torch
 from accelerate.utils import broadcast
 
 import wandb
@@ -61,8 +62,8 @@ def train(
         stop = False
         if accelerator.is_main_process:
             stop = early_stopping.should_stop(val_res)
-        stop = broadcast(stop)
-        if stop:
+        stop = broadcast(torch.tensor(stop, dtype=torch.bool))
+        if stop.item():
             print(f"Early stopping triggered in epoch {epoch + 1}")
             break
     epoch_evaluate(